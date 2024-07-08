83f07a25a9d16ccf37d116ce0af48f61938e9b93
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-07-08 23:01:20 +0800

    add function(+-*/)

--- 0708Calculator/app/src/main/java/com/example/a0708calculator/MainActivity.java
+++ 0708Calculator/app/src/main/java/com/example/a0708calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a0708calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    // 加法
+    public void add(View view) {
+        TextView num1Text = (TextView) findViewById(R.id.num1);
+        TextView num2Text = (TextView) findViewById(R.id.num2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double num1 = Double.parseDouble(num1Text.getText().toString());
+        double num2 = Double.parseDouble(num2Text.getText().toString());
+        double add = num1 + num2;
+        result.setText(String.valueOf(add));
+    }
+
+    // 減法
+    public void sub(View view) {
+        TextView num1Text = (TextView) findViewById(R.id.num1);
+        TextView num2Text = (TextView) findViewById(R.id.num2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double num1 = Double.parseDouble(num1Text.getText().toString());
+        double num2 = Double.parseDouble(num2Text.getText().toString());
+        double sub = num1 - num2;
+        result.setText(String.valueOf(sub));
+    }
+    
+    // 乘法
+    public void multi(View view) {
+        TextView num1Text = (TextView) findViewById(R.id.num1);
+        TextView num2Text = (TextView) findViewById(R.id.num2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        double num1 = Double.parseDouble(num1Text.getText().toString());
+        double num2 = Double.parseDouble(num2Text.getText().toString());
+        double multi = num1 * num2;
+        result.setText(String.valueOf(multi));
+    }
+    
+    // 除法
+    public void mod(View view) {
+        TextView num1Text = (TextView) findViewById(R.id.num1);
+        TextView num2Text = (TextView) findViewById(R.id.num2);
+        TextView result = (TextView) findViewById(R.id.result);
+        Button modBtn = (Button) findViewById(R.id.modBtn);
+
+        double num1 = Double.parseDouble(num1Text.getText().toString());
+        double num2 = Double.parseDouble(num2Text.getText().toString());
+        
+        if (num2 != 0) {
+            double mod = num1 / num2;
+            result.setText(String.valueOf(mod));
+            
+        } else {
+            result.setText("num2不能為0");
+            modBtn.setEnabled(false);
+        }
     }
 }
