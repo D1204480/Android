f75e90c28507e644efd092865fdf07536f72ce6b
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-07-17 17:44:49 +0800

    update

--- 0717bank_app/app/src/main/java/com/example/a0717bank_app/ExchangeActivity.java
+++ 0717bank_app/app/src/main/java/com/example/a0717bank_app/ExchangeActivity.java
@@ -1,8 +1,6 @@
 package com.example.a0717bank_app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ExchangeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_exchange);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void GotoDeposit(View view) {
-        Intent intent = new Intent(this, DepositActivity.class);
-        startActivity(intent);
-    }
-
-    public void GotoYenExchange(View view) {
-        Intent intent = new Intent(this, ExchangeActivity.class);
-        startActivity(intent);
     }
 }
