4402fbbd104f001464994bcbe05fa59ff282a9ce
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-07-20 17:43:07 +0800

    update

--- 0717bank_app/app/src/main/java/com/example/a0717bank_app/DepositActivity.java
+++ 0717bank_app/app/src/main/java/com/example/a0717bank_app/DepositActivity.java
@@ -1,14 +1,25 @@
 package com.example.a0717bank_app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DepositActivity extends AppCompatActivity {
+
+    private double dollarNTD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,59 @@ public class DepositActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+//        // 接受資料
+//        Bundle bundle = this.getIntent().getExtras();
+//
+//        // 取得台幣餘額
+//        if (bundle != null) {
+//            String NTD = String.format(bundle.getString("NTD"));
+//            dollarNTD = Double.parseDouble(NTD);
+//        }
+
+
+        // 接收來自MainActivity的台幣於惡資料
+        Intent intent = getIntent();
+        dollarNTD = intent.getDoubleExtra("NTD", 0);
+
+
     }
+
+
+    // 取款、存款
+    public void handleConfirm(View view) {
+        // 讀取輸入金額
+        EditText inputMoneyText = (EditText) findViewById(R.id.input_money);
+        String inputMoneyStr = inputMoneyText.getText().toString();
+
+        if (!inputMoneyStr.isEmpty()) {
+            double inputMoney = Double.parseDouble(inputMoneyText.getText().toString());
+
+            if (view.getId() == R.id.withdraw_btn) {   // 取款
+
+                if (dollarNTD >= inputMoney) {
+                    dollarNTD = dollarNTD - inputMoney;
+                }
+
+            } else if (view.getId() == R.id.deposit_btn) {   // 存款
+
+                dollarNTD = dollarNTD + inputMoney;
+            }
+
+            // 回傳更新後的餘額
+            Intent resultIntent = new Intent();  // 跳轉回mainActivity
+            resultIntent.putExtra("NTD", dollarNTD);  // 資料傳回main
+            setResult(Activity.RESULT_OK, resultIntent);
+
+            // 餘額不足
+            if (dollarNTD < 0) {
+                setResult(Activity.RESULT_CANCELED, resultIntent);
+            }
+
+            finish();
+        }
+    }
+
+
 }
--- 0717bank_app/app/src/main/java/com/example/a0717bank_app/MainActivity.java
+++ 0717bank_app/app/src/main/java/com/example/a0717bank_app/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.a0717bank_app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
+    private double dollarNTD  = 1000; // 帳戶餘額，預設1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +32,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        intentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        // 寫另一個Activity回傳後, 得到回傳的資料之後的方法
+                        if (result.getData() != null && result.getResultCode() == Activity.RESULT_OK) {
+                            dollarNTD = result.getData().getDoubleExtra("NTD", -1);
+
+                            updateUI();
+                        }
+                    }
+                }
+        );
     }
 
     public void GotoDeposit(View view) {
         Intent intent = new Intent(this, DepositActivity.class);
-        startActivity(intent);
+        intent.putExtra("NTD", dollarNTD);
+//        startActivity(intent);
+        intentActivityResultLauncher.launch(intent);
     }
 
     public void GotoYenExchange(View view) {
         Intent intent = new Intent(this, ExchangeActivity.class);
         startActivity(intent);
     }
+
+
+    // 更新台幣餘額
+    public void updateUI() {
+        TextView textResult = (TextView) findViewById(R.id.showNTD);
+        textResult.setText(String.valueOf(dollarNTD));
+
+    }
+
+
+//    public void SentNTD(View view){
+//
+//        // 傳送NTD餘額
+//        String dollarNTD;
+//        TextView NTD = (TextView) findViewById(R.id.showNTD);
+//        dollarNTD = NTD.toString();
+//
+//        // 建立Intent
+//        Intent intent = new Intent(this, DepositActivity.class);
+//
+//        // 設定一個bundle來放資料
+//        Bundle bundle = new Bundle();
+//        bundle.putString("NTD", dollarNTD);
+//
+//        // 利用intent攜帶bundle資料
+//        intent.putExtras(bundle);
+//        startActivity(intent);
+//
+//    }
 }
